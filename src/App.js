eda220311661247cd1da6a72d42552f224db3e49
RocketKevin <kevinchen91200@gmail.com>
2020-07-10 12:32:56 -0400

    0.0.1

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import './App.css';
+
+/*
+function input() {
+  let num = 0;
+
+}
+*/
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+  render() {
+
+    //let num = Math.floor(Math.random() * 9) + 1;
+    
+    return (
+      <input 
+        type="" 
+        className="square"
+        maxlength="1"
+        value={this.state.inputValue}
+        onChange={event => this.setState({inputValue: event.target.value.replace(/\D/,'')})}
+      >
+      </input>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+
+    let oneByThree = [];
+    let threeByThree = [];
+    let threeByNine = [];
+    let nineByNine = [];
+
+    //1x3 board
+    for(let i = 0; i < 3; i++){
+      oneByThree.push(
+        <div>
+          {this.renderSquare(i)}
+        </div>
+      );
+    }
+
+    //3x3 board
+    for(let i = 0; i < 3; i++){
+      threeByThree.push(
+        <div className="board-row">
+          {oneByThree}
+        </div>
+      );
+    }
+
+    //3x9 board
+    for(let i = 0; i < 3; i++){
+      threeByNine.push(
+        <div className="side">
+          {threeByThree}
+        </div>
+      );
+    }
+
+    //9x9 board
+    for(let i = 0; i < 3; i++){
+      nineByNine.push(
+        <div>
+          {threeByNine}
+        </div>
+      );
+    }
+
+    return (
+      <div className="gameBoard">
+        {nineByNine}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <p>
+          Sudoku!
+        </p>
+
+        <div className="game-board">
+          <Board />
+        </div>
+
+        <button id="submit">
+          Submit
+        </button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
