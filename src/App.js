779ffe0c4870397edc480f31bc2a88361c0b8daf
sliang100 <sherry.liang100@gmail.com>
2020-07-22 16:47:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ class Square extends React.Component {
     };
   }
   render() {
-    
+
     //Get variables from outside
     let val = this.props.displayValue;
 
     //If there is no value, player can enter value
-    if(this.props.displayValue === 0) {
+    if (this.props.displayValue === 0) {
       val = this.state.inputValue;
     }
 
     return (
 
       //Each square is an input box 
-      <input 
-        type="" 
+      <input
+        type=""
         className="square"
 
         //Only one letter, number, or character
@@ -35,30 +35,35 @@ class Square extends React.Component {
         }
 
         //Only number can be type
-        onChange={event => this.setState({inputValue: event.target.value.replace(/\D/,'')})}
+        onChange={event => this.setState({ inputValue: event.target.value.replace(/\D/, '') })}
       >
       </input>
     );
   }
 }
 
+class CheckSolution extends React.Component {
+
+  checkRow()
+}
+
 class Board extends React.Component {
 
   //Create a square
   renderSquare(num) {
-    return <Square 
-      displayValue = {num}
+    return <Square
+      displayValue={num}
     />;
   }
 
   // given a sudoku position, returns the row
   returnRow(position) {
-	  return Math.floor(position / 9);
+    return Math.floor(position / 9);
   }
 
   // given a sudoku position, returns the column
   returnCol(position) {
-	  return position % 9;
+    return position % 9;
   }
 
   //Gives 3 by 3 block
@@ -68,18 +73,18 @@ class Board extends React.Component {
 
   //Check for conflict in row
   isPossibleRow(randomNumber, y, valueBoard) {
-    for(let i = 0; i < 9; i++) {
-      if(parseInt(valueBoard[y * 9 + i]) === randomNumber) {
+    for (let i = 0; i < 9; i++) {
+      if (parseInt(valueBoard[y * 9 + i]) === randomNumber) {
         return false;
       }
     }
     return true;
   }
-  
+
   //Check for conflict in col
   isPossibleCol(randomNumber, x, valueBoard) {
     for (let i = 0; i < 9; i++) {
-      if(parseInt(valueBoard[i * 9 + x]) === randomNumber) {
+      if (parseInt(valueBoard[i * 9 + x]) === randomNumber) {
         return false;
       }
     }
@@ -88,11 +93,11 @@ class Board extends React.Component {
 
   //Checks 3 by 3 block
   isPossibleBlock(randomNumber, position, valueBoard) {
-   
+
     //Iterate through the 3 by 3 block
     for (let i = 0; i < 9; i++) {
       let oneOfTheNumberInThreeByThreeBlock = Math.floor(position / 3) * 27 + i % 3 + 9 * Math.floor(i / 3) + 3 * (position % 3);
-      
+
       //If one of the number in 3 by 3 block is the same as the random number
       if (parseInt(valueBoard[oneOfTheNumberInThreeByThreeBlock]) === randomNumber) {
         return false;
@@ -105,10 +110,10 @@ class Board extends React.Component {
   //Change the possibleNumberBoard and valueBoard
   positionGenerator(i, possibleNumberBoard, valueBoard) {
 
-    if(possibleNumberBoard[i].length === 0) {
+    if (possibleNumberBoard[i].length === 0) {
 
       //If there are no possble numbers left, refill
-      possibleNumberBoard[i] = [1,2,3,4,5,6,7,8,9];
+      possibleNumberBoard[i] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
       valueBoard[i] = 0;
       valueBoard[i - 1] = 0;
@@ -119,18 +124,18 @@ class Board extends React.Component {
 
       //Get a random number from array
       let randomNumberOfArray = possibleNumberBoard[i][Math.floor(Math.random() * possibleNumberBoard[i].length)];
-      
+
       //Checks for confliction for row, col, and block
-      if(
-        this.isPossibleRow(randomNumberOfArray, this.returnRow(i), valueBoard) && 
-        this.isPossibleCol(randomNumberOfArray, this.returnCol(i), valueBoard) && 
+      if (
+        this.isPossibleRow(randomNumberOfArray, this.returnRow(i), valueBoard) &&
+        this.isPossibleCol(randomNumberOfArray, this.returnCol(i), valueBoard) &&
         this.isPossibleBlock(randomNumberOfArray, this.returnBlock(i), valueBoard)
       ) {
 
         //If no confliction use the random number
         valueBoard[i] = [randomNumberOfArray];
         let index = possibleNumberBoard[i].indexOf(randomNumberOfArray);
-        if(index >= 0) {
+        if (index >= 0) {
           possibleNumberBoard[i].splice(index, 1);
         }
 
@@ -140,8 +145,8 @@ class Board extends React.Component {
 
         //If conflicts, remove that possible number
         let indexOne = possibleNumberBoard[i].indexOf(randomNumberOfArray);
-        if(indexOne >= 0) {
-          possibleNumberBoard[i].splice(indexOne,1);
+        if (indexOne >= 0) {
+          possibleNumberBoard[i].splice(indexOne, 1);
         }
 
         valueBoard[i] = 0;
@@ -155,26 +160,26 @@ class Board extends React.Component {
   render() {
 
     //For display
-    let board = [];  
+    let board = [];
 
     //Holds possible values of a square
     let possibleNumberBoard = [];
 
     //Holds one true value
     let valueBoard = [];
-    
+
     let index = 0;
 
-    while(index < 81) {
+    while (index < 81) {
 
-      possibleNumberBoard.push([1,2,3,4,5,6,7,8,9]);
+      possibleNumberBoard.push([1, 2, 3, 4, 5, 6, 7, 8, 9]);
       valueBoard.push(0);
 
       //Get new postion of index
       let position = this.positionGenerator(index, possibleNumberBoard, valueBoard);
       let precheck = index + position;
 
-      if(precheck >= 0) {
+      if (precheck >= 0) {
 
         //Where index is at
         index += position;
@@ -185,8 +190,8 @@ class Board extends React.Component {
     }
 
     //Store all into board
-    for(let i = 0; i < 9; i++) {
-      for(let j = 0; j < 9; j++) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
         board.push(
           <div>
             {this.renderSquare(valueBoard[i * 9 + j])}
@@ -194,7 +199,7 @@ class Board extends React.Component {
         )
       }
     }
-    
+
     return (
       <div className="gameBoard">
         {board}
@@ -207,7 +212,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+
         <h1 id="title">
           Sudoku!
         </h1>
@@ -215,7 +220,7 @@ class App extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        
+
         <button id="submit">
           Submit
         </button>
