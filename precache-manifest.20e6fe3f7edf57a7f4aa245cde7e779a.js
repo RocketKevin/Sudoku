3fac094096cb14dcd8e340a6eaa4b9b5b9a2b27c
RocketKevin <kevinchen91200@gmail.com>
2020-07-28 21:47:09 -0400

    Updates

---
+++ precache-manifest.20e6fe3f7edf57a7f4aa245cde7e779a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc3afcb33de1fcf33a346443b0f1b6f",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "66ff9a1771591f646429",
+    "url": "/Sudoku/static/css/main.5fbee68f.chunk.css"
+  },
+  {
+    "revision": "3ef40b00ac6d8a8394af",
+    "url": "/Sudoku/static/js/2.7a6d3530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sudoku/static/js/2.7a6d3530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ff9a1771591f646429",
+    "url": "/Sudoku/static/js/main.d772de2b.chunk.js"
+  },
+  {
+    "revision": "070d804769e422107ec9",
+    "url": "/Sudoku/static/js/runtime-main.3f9494c9.js"
+  }
+]);
