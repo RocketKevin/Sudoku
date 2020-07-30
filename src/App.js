5e5406c77420e0cfcdcea33450436ccef1f5d28d
RocketKevin <kevinchen91200@gmail.com>
2020-07-30 14:28:50 -0400

    Added medium and select box

--- src/App.js
+++ src/App.js
@@ -146,10 +146,30 @@ function easy() {
   window.location.reload();
 }
 
+function medium() {
+  var mode = "medium";
+  localStorage.setItem("mode", mode);
+  window.location.reload();
+}
+
 function hard() {
   var mode = "hard";
   localStorage.setItem("mode", mode);
   window.location.reload();
+}
+
+function generate() {
+  if(document.readyState === "complete") {
+    let selectMenu = document.getElementById("selectMenu");
+    let modeVal = selectMenu.options[selectMenu.selectedIndex].value;
+    if(parseInt(modeVal) === 1) {
+      easy();
+    } else if(parseInt(modeVal) === 2) {
+      medium();
+    } else if(parseInt(modeVal) === 3) {
+      hard();
+    }
+  }
 }
 
 //Making individual squares
@@ -395,6 +415,8 @@ class Board extends React.Component {
     let amountRemove = 46;
     if(mode === "easy") {
       amountRemove = 15;
+    } else if(mode === "medium") {
+      amountRemove = 30;
     }
     while(index < amountRemove) {
       //Remove number and obtain it's value and position
@@ -522,24 +544,41 @@ class Box extends React.Component {
   render() {
 
     let text = "Something isn't right. Keep going!";
-
-    if(stateOfWorld()) {
-      text = "You Win";
-    }
-
-    let box = (
-      <div id="dialog">
-        <div>
-          {text}
+    let box = null;
+    if(this.props.openSubmit) { 
+      if(stateOfWorld()) {
+        text = "You Win";
+      }
+  
+      box = (
+        <div id="dialog">
+          <div>
+            {text}
+          </div>
+          <button id="close" onClick = {this.props.closeSubmit}>
+            Close
+          </button>
         </div>
-        <button id="close" onClick = {this.props.close}>
-          Close
-        </button>
-      </div>
-    );
-
-    if(!this.props.open) {
-      box = null;
+      );
+    } else if(this.props.openNewGame) {
+      box = (
+        <div id="dialog">
+          <div class="custom-select">
+            <select id="selectMenu">
+              <option value="0">Select Mode:</option>
+              <option value="1">Easy</option>
+              <option value="2">Medium</option>
+              <option value="3">Hard</option>
+            </select>
+          </div>
+          <button id="close" onClick = {generate}>
+            Generate
+          </button>
+          <button id="close" onClick = {this.props.closeNewGame}>
+            Close
+          </button>
+        </div>
+      );
     }
 
     return(
@@ -553,22 +592,25 @@ class Box extends React.Component {
 class Button extends React.Component {
 
   state = {
-    open: false
+    openSubmit: false,
+    openNewGame: false
   }
 
   render() {
     return(
       <div>
-        <button className="button" onClick={(e) => this.setState({open: true})}>
+        <button className="button" onClick={(e) => this.setState({openSubmit: true})}>
           Submit
         </button>
-        <button className="button" onClick={easy}>
-          Easy
+        <button className="button" onClick={(e) => this.setState({openNewGame: true})}>
+          New Game
         </button>
-        <button className="button" onClick={hard}>
-          Hard
-        </button>
-        <Box open = {this.state.open} close = {(e) => this.setState({open: false})}/>
+        <Box 
+          openSubmit = {this.state.openSubmit} 
+          closeSubmit = {(e) => this.setState({openSubmit: false})}
+          openNewGame = {this.state.openNewGame} 
+          closeNewGame = {(e) => this.setState({openNewGame: false})}
+        />
       </div>
     )
   }
